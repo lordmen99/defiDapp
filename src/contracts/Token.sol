2ddd4f88a7256f098b6f3241b5b29861c67ec99e
Daniel Lord-Doyle <daniel@Daniels-MBP.modem>
2022-05-22 12:30:16 +1000

    adding in tests and artifacts

--- src/contracts/Token.sol
+++ src/contracts/Token.sol
@@ -5,17 +5,29 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Token is ERC20 {
   //add minter variable
+  address public minter;
 
   //add minter changed event
+  event MinterChanged(address indexed from, address to);
 
-  constructor() public payable ERC20("Name", "Symbol") {
+  constructor() public payable ERC20("The Lords Currency", "CRX") {
     //asign initial minter
+    minter = msg.sender;
   }
 
   //Add pass minter role function
+  function passMinterRole(address dBank) public returns (bool) {
+    require(msg.sender == minter, 'Error, must be minter to swap minter roles.');
+    minter = dBank;
+
+    emit MinterChanged(msg.sender, dBank);
+    return true;
+  }
 
   function mint(address account, uint256 amount) public {
     //check if msg.sender have minter role
+    require(msg.sender == minter, "Error, you are not the minter");
 		_mint(account, amount);
 	}
+
 }
